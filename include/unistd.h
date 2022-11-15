23a67d964c29e9fd69b5a27b63086e2855d0aa24
谭源 <tanyuan98@outlook.com>
2022-11-15 18:00:06 +0800

    fix: declaration problem

--- include/unistd.h
+++ include/unistd.h
@@ -207,6 +207,7 @@ int execle(const char * pathname, char * arg0, ...);
 volatile void exit(int status);
 volatile void _exit(int status);
 int fcntl(int fildes, int cmd, ...);
+static int fork(void);
 int getpid(void);
 int getuid(void);
 int geteuid(void);
@@ -220,6 +221,7 @@ int mknod(const char * filename, mode_t mode, dev_t dev);
 int mount(const char * specialfile, const char * dir, int rwflag);
 int nice(int val);
 int open(const char * filename, int flag, ...);
+static int pause(void);
 int pipe(int * fildes);
 int read(int fildes, char * buf, off_t count);
 int setpgrp(void);
@@ -230,6 +232,7 @@ void (*signal(int sig, void (*fn)(int)))(int);
 int stat(const char * filename, struct stat * stat_buf);
 int fstat(int fildes, struct stat * stat_buf);
 int stime(time_t * tptr);
+static int sync(void);
 time_t time(time_t * tloc);
 time_t times(struct tms * tbuf);
 int ulimit(int cmd, long limit);
@@ -246,11 +249,5 @@ int dup2(int oldfd, int newfd);
 int getppid(void);
 pid_t getpgrp(void);
 pid_t setsid(void);
-
-#ifndef __KERNEL_MAIN__
-int fork(void);
-int pause(void);
-int sync(void);
-#endif
 
 #endif
